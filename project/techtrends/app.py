e917cf1feec2303a991e5e463347d564fa7ce1b5
charlyliu <liu.chen.archer@gmail.com>
2025-01-23 16:36:38 +0800

    update github actions

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -3,11 +3,18 @@ import sqlite3
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+import logging
+
+# Global variable to keep track of the number of database connections
+db_connection_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -35,7 +42,10 @@ def index():
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
+
+    app.logger.debug('Article "' + post['title'] + '" retrieved!')
     if post is None:
+      app.logger.debug('A non-existing article is accessed 404')
       return render_template('404.html'), 404
     else:
       return render_template('post.html', post=post)
@@ -43,6 +53,7 @@ def post(post_id):
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.debug('"About Us" page is retrieved')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -61,9 +72,37 @@ def create():
             connection.commit()
             connection.close()
 
+            app.logger.debug('Article "' + title + '" created!')
+            
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
+@app.route('/healthz')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info('Status request successfull')
+    return response
+
+@app.route('/metrics')
+def metrics():
+
+    connection = get_db_connection()
+
+    post_count = connection.execute('SELECT COUNT(*) FROM posts').fetchone()[0]
+    connection.close()
+    
+    response = app.response_class(
+            response=json.dumps({"db_connection_count": db_connection_count, "post_count": post_count}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info('Metrics request successfull')
+    return response
 
 # start the application on port 3111
 if __name__ == "__main__":
